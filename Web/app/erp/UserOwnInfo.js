19c6668aaaa0f7b7949cf345a860e297e436f029
YaoQ <245819252@qq.com>
2015-07-20 09:35:42 +0800

    123

--- Web/app/erp/UserOwnInfo.js
+++ Web/app/erp/UserOwnInfo.js
@@ -1,1 +1,16 @@
 ﻿//个人信息维护
+Ext.define('TCSYS.erp.UserOwnInfo', {
+    extend: 'Ext.panel.Panel',
+    title: '客户资料',
+    name: 'SaleCustomer',
+    alias: "widget.UserOwnInfo",
+    closable: true,
+    layout: {
+        type: 'vbox',
+        align: 'stretch'
+    },
+    initComponent: function () {
+        this.callParent(arguments);
+        var me = this;
+    }
+});
